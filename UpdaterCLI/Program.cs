9c47aab5301108d0b196fc0a5a2310185b3c4bf3
Andrew <150041284+ndrwcube@users.noreply.github.com>
2024-08-05 18:35:15 +0800

    Added main Install functionality

--- UpdaterCLI/Program.cs
+++ UpdaterCLI/Program.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Ionic.Zip;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace UpdaterCLI
@@ -9,6 +15,65 @@ namespace UpdaterCLI
     {
         static void Main(string[] args)
         {
+            if (args.Length >= 2)
+            {
+                if (args[0] == "install")
+                {
+                    Install(args[1]);
+                }
+                else if (args[0] == "uninstall")
+                {
+                    Uninstall(args[1]);
+                } else
+                {
+                    Console.WriteLine("Unrecognised command");
+                }
+            } else
+            {
+                Console.WriteLine();
+            }
+        }
+
+        static void Install(string id)
+        {
+            if (PkgIndexObj().ContainsKey(id))
+            {
+                if (!Directory.Exists(@"C:\BastionSG\Updater\DL\" + id))
+                {
+                    Directory.CreateDirectory(@"C:\BastionSG\");
+                    Directory.CreateDirectory(@"C:\BastionSG\" + id);
+                    Directory.CreateDirectory(@"C:\BastionSG\Updater\DL\" + id);
+                }
+                new WebClient().DownloadFile("https://raw.githubusercontent.com/BastionDevs/BastionUpdater/main/pkg/"+id+"/latest/download.zip", @"C:\BastionSG\Updater\DL\"+id+@"\download.zip");
+                
+                using (ZipFile zip = ZipFile.Read(@"C:\BastionSG\Updater\DL\" + id + @"\download.zip"))
+                {
+                    zip.ExtractAll(@"C:\BastionSG\" + id);
+                }
+            } else
+            {
+                Console.WriteLine("Invalid package ID");
+            }
+        }
+
+        static void Uninstall(string id)
+        {
+            if (!Directory.Exists(@"C:\BastionSG\"+id))
+            {
+                Console.WriteLine("Package not installed");
+            } else
+            {
+                Directory.Delete(@"C:\BastionSG\"+id, true);
+                Console.WriteLine("Package uninstalled");
+            }
+        }
+
+        static JObject PkgIndexObj()
+        {
+            using (WebClient client = new WebClient())
+            {
+                return JObject.Parse(client.DownloadString("https://raw.githubusercontent.com/BastionDevs/BastionUpdater/main/packages.json"));
+            }
         }
     }
 }
